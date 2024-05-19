0d4b4d05aa7a41a85d66a4944b8a4d1b69eab438
Mas1917 <msidiropoulos24@yahoo.com>
2024-05-18 20:36:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,15 @@ app.get('/', async (req, res) => {
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/add', async (req, res) => {
+
+    try {
+        res.render('add', {});
+        
+    } catch(err) {
+        console.error(err);
+    }
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
